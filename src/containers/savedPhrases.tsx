92e8f80d1777aad222baf76cbe031d73f4b990ca
Ryan Felone <ryanfelone@gmail.com>
2020-03-20 23:45:57 -0400

    removal working

--- src/containers/savedPhrases.tsx
+++ src/containers/savedPhrases.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput} from 'react-native';
 import * as PropTypes from 'prop-types';
 import {connect, Dispatch} from 'react-redux';
 import {
@@ -14,9 +14,11 @@ import {
   removeSavedPhrase,
   updateSavedPhrase,
 } from '../store/saved-phrase-list/actions';
-import { AnyAction } from 'redux';
+import {AnyAction} from 'redux';
 
-export interface SavedPhrasesState {}
+export interface SavedPhrasesState {
+  newPhraseText: string;
+}
 
 export interface SavedPhrasesProps {
   navigation: object;
@@ -36,19 +38,30 @@ class SavedPhrases extends React.Component<
 
   constructor(props: SavedPhrasesProps) {
     super(props);
+    this.state = {
+      newPhraseText: '',
+    };
   }
+
+  onAddButton = () => {
+    this.props.addSavedPhrase(this.state.newPhraseText, new Date(), new Date());
+    this.setState({newPhraseText: ''});
+  };
 
   render() {
     return (
       <View>
-        <Text>TEST THIS SHITS</Text>
-        <Button
-          title="testTitle"
-          onPress={() =>
-            this.props.addSavedPhrase('Test', '12-20-2020', '12-20-2020')
-          }
+        <TextInput
+          placeholder="Type here to translate!"
+          onChangeText={newPhraseText => this.setState({newPhraseText})}
+          value={this.state.newPhraseText}
         />
-        <Text>{JSON.stringify(this.props.savedPhrases)}</Text>
+        <Button title="Add Phrase" onPress={() => this.onAddButton()} />
+        {this.props.savedPhrases.map((item, key) => (
+          <Text key={key} onPress={() => this.props.removeSavedPhrase(key)}>
+            {item.statement}
+          </Text>
+        ))}
       </View>
     );
   }
@@ -62,6 +75,7 @@ const mapDispatchToProps = (dispatch: Dispatch<AnyAction>) => {
   return {
     addSavedPhrase: (statement: string, createdAt: string, updatedAt: string) =>
       dispatch(addSavedPhrase(statement, createdAt, updatedAt)),
+    removeSavedPhrase: (index: number) => dispatch(removeSavedPhrase(index)),
   };
 };
 
